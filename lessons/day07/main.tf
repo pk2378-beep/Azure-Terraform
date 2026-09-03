3999837c57a37e0825dc2d0758ce0c9891467ce7
pk2378-beep <pk2378@gmail.com>
2026-09-03 13:57:46 +0530

    final

--- lessons/day07/main.tf
+++ lessons/day07/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_virtual_machine" "main" {
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = var.allowed_vm_sizes[0]
+  vm_size               = var.allowed_vm_sizes[1]
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = var.vm_delete_os_disk_on_termination
